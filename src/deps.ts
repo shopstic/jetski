8f78eaa30ba4d2c532ff6ee89e1adef9f964fdf1
Jacky Nguyen <nktpro@gmail.com>
2023-01-27 15:46:18 -0500

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
-export * from "https://deno.land/x/utils@2.8.0/cli_utils.ts";
-export * from "https://deno.land/x/utils@2.8.0/exec_utils.ts";
-export * from "https://deno.land/x/utils@2.8.0/deps/typebox.ts";
-export * from "https://deno.land/x/utils@2.8.0/validation_utils.ts";
-export type { ValidationResult } from "https://deno.land/x/utils@2.8.0/validation_utils.ts";
-export * from "https://deno.land/std@0.155.0/fmt/colors.ts";
+export * from "https://deno.land/x/utils@2.9.0/cli_utils.ts";
+export * from "https://deno.land/x/utils@2.9.0/exec_utils.ts";
+export * from "https://deno.land/x/utils@2.9.0/deps/typebox.ts";
+export * from "https://deno.land/x/utils@2.9.0/validation_utils.ts";
+export type { ValidationResult } from "https://deno.land/x/utils@2.9.0/validation_utils.ts";
+export * from "https://deno.land/std@0.172.0/fmt/colors.ts";
 
 export {
   basename,
@@ -12,13 +12,13 @@ export {
   fromFileUrl,
   join as joinPath,
   resolve as resolvePath,
-} from "https://deno.land/std@0.155.0/path/mod.ts";
+} from "https://deno.land/std@0.172.0/path/mod.ts";
 
-export { parse as parseYaml, stringify as stringifyYaml } from "https://deno.land/std@0.155.0/encoding/yaml.ts";
-export type { YAMLError } from "https://deno.land/std@0.155.0/encoding/_yaml/error.ts";
+export { parse as parseYaml, stringify as stringifyYaml } from "https://deno.land/std@0.172.0/encoding/yaml.ts";
+export type { YAMLError } from "https://deno.land/std@0.172.0/encoding/_yaml/error.ts";
 
-export { exists as fsExists } from "https://deno.land/std@0.155.0/fs/exists.ts";
-export { memoizePromise } from "https://deno.land/x/utils@2.8.0/async_utils.ts";
+export { exists as fsExists } from "https://deno.land/std@0.172.0/fs/exists.ts";
+export { memoizePromise } from "https://deno.land/x/utils@2.9.0/async_utils.ts";
 
-export { delay } from "https://deno.land/std@0.155.0/async/mod.ts";
-export { iterateReader, writeAll, writeAllSync } from "https://deno.land/std@0.155.0/streams/conversion.ts";
+export { delay } from "https://deno.land/std@0.172.0/async/mod.ts";
+export { iterateReader, writeAll, writeAllSync } from "https://deno.land/std@0.172.0/streams/conversion.ts";
