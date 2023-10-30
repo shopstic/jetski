fe2ca3e5662769dee7e6539e3e6bdc3e47ffd466
Jacky Nguyen <nktpro@gmail.com>
2023-10-30 08:08:15 -0400

    Use chrony and time.aws.com

--- scripts/cloud_init/update_timesyncd.sh
+++ scripts/cloud_init/update_timesyncd.sh
@@ -2,13 +2,48 @@
 
 set -euo pipefail
 
-cat >/etc/systemd/timesyncd.conf <<EOF
-[Time]
-NTP=time.aws.com
-FallbackNTP=ntp.ubuntu.com
-PollIntervalMinSec=5
-PollIntervalMaxSec=60
+systemctl disable --now systemd-timesyncd
+apt install -y chrony
+
+cat >/etc/chrony/chrony.conf <<EOF
+pool time.aws.com iburst
+
+# Use time sources from DHCP.
+sourcedir /run/chrony-dhcp
+
+# This directive specify the location of the file containing ID/key pairs for
+# NTP authentication.
+keyfile /etc/chrony/chrony.keys
+
+# This directive specify the file into which chronyd will store the rate
+# information.
+driftfile /var/lib/chrony/chrony.drift
+
+# Save NTS keys and cookies.
+ntsdumpdir /var/lib/chrony
+
+# Uncomment the following line to turn logging on.
+#log tracking measurements statistics
+
+# Log files location.
+logdir /var/log/chrony
+
+# Stop bad estimates upsetting machine clock.
+maxupdateskew 100.0
+
+# This directive enables kernel synchronisation (every 11 minutes) of the
+# real-time clock. Note that it can’t be used along with the 'rtcfile' directive.
+rtcsync
+
+# Step the system clock instead of slewing it if the adjustment is larger than
+# one second, but only in the first three clock updates.
+makestep 1 3
+
+# Get TAI-UTC offset and leap seconds from the system tz database.
+# This directive must be commented out when using time sources serving
+# leap-smeared time.
+leapsectz right/UTC
 EOF
 
-systemctl restart systemd-timesyncd
-timedatectl status
+service chrony force-reload
+chronyc tracking
