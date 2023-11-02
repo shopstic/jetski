9abe164eaefe014fc679859c96445fa2d801c68c
Jacky Nguyen <nktpro@gmail.com>
2023-11-02 16:39:51 -0400

    Update dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
-export * from "https://deno.land/x/utils@2.17.0/cli_utils.ts";
-export * from "https://deno.land/x/utils@2.17.0/exec_utils.ts";
-export * from "https://deno.land/x/utils@2.17.0/validation_utils.ts";
+export * from "https://deno.land/x/utils@2.18.1/cli_utils.ts";
+export * from "https://deno.land/x/utils@2.18.1/exec_utils.ts";
+export * from "https://deno.land/x/utils@2.18.1/validation_utils.ts";
 export * from "./deps/typebox.ts";
-export type { ValidationResult } from "https://deno.land/x/utils@2.17.0/validation_utils.ts";
-export * from "https://deno.land/std@0.202.0/fmt/colors.ts";
+export type { ValidationResult } from "https://deno.land/x/utils@2.18.1/validation_utils.ts";
+export * from "https://deno.land/std@0.205.0/fmt/colors.ts";
 
 export {
   basename,
@@ -12,14 +12,14 @@ export {
   fromFileUrl,
   join as joinPath,
   resolve as resolvePath,
-} from "https://deno.land/std@0.202.0/path/mod.ts";
+} from "https://deno.land/std@0.205.0/path/mod.ts";
 
-export { parse as parseYaml, stringify as stringifyYaml } from "https://deno.land/std@0.202.0/yaml/mod.ts";
-export type { YAMLError } from "https://deno.land/std@0.202.0/yaml/_error.ts";
+export { parse as parseYaml, stringify as stringifyYaml } from "https://deno.land/std@0.205.0/yaml/mod.ts";
+export type { YAMLError } from "https://deno.land/std@0.205.0/yaml/_error.ts";
 
-export { exists as fsExists } from "https://deno.land/std@0.202.0/fs/exists.ts";
-export { ensureFile } from "https://deno.land/std@0.202.0/fs/ensure_file.ts";
-export { assertExists } from "https://deno.land/std@0.202.0/assert/assert_exists.ts";
-export { memoizePromise } from "https://deno.land/x/utils@2.17.0/async_utils.ts";
+export { exists as fsExists } from "https://deno.land/std@0.205.0/fs/exists.ts";
+export { ensureFile } from "https://deno.land/std@0.205.0/fs/ensure_file.ts";
+export { assertExists } from "https://deno.land/std@0.205.0/assert/assert_exists.ts";
+export { memoizePromise } from "https://deno.land/x/utils@2.18.1/async_utils.ts";
 
-export { delay } from "https://deno.land/std@0.202.0/async/delay.ts";
+export { delay } from "https://deno.land/std@0.205.0/async/delay.ts";
